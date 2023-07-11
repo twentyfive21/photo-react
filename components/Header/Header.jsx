ab99a015821733b65e4d8216f811ee27086fdc89
Tina <tina.winnn@gmail.com>
2023-07-10 21:59:09 -0400

    added header styling

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Header.css'
+import { TbCameraHeart } from "react-icons/tb";
 
 function Header() {
   return (
     <div className='header-container'>
-        Header
+        <section className='header-left'>
+        <TbCameraHeart className='camera'/>
+        <h1>Polaroid Palette</h1>
+        </section>
+        <h2>My Palette</h2>
     </div>
   )
 }
