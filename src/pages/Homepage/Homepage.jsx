2c22eefa7fa4d7c13c2ab965284086eecbe37617
Tina <tina.winnn@gmail.com>
2023-07-11 20:51:28 -0400

    fetch for curated photos

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Homepage.css'
 import banner from '../../assets/banner.jpg'
+import axios from 'axios'
 
 
 function Homepage() {
+
+    // create state for page number
+    const [photos, setPhotos] = useState([])
+    const pageNum = 1;
+
+// Make a api call to site
+useEffect(() => {
+
+  const fetchData = async () => {
+    try {
+      const apiKey = import.meta.env.VITE_API_KEY
+      const response = await fetch(`https://api.pexels.com/v1/curated?page=${pageNum}&per_page=12`, {
+        headers: {
+          Authorization: apiKey
+        }
+      });
+      const jsonData = await response.json();
+      setPhotos(jsonData.photos)
+     console.log(photos)
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  fetchData();
+}, []);
+
+
   return (
     <div className='homepage-container'>
-      <img src={banner} className='homepage-banner'/>
+      <button>Prev</button>
+      <button>Next</button>
+      {/* <img src={banner} className='homepage-banner'/> */}
     </div>
     
   )
