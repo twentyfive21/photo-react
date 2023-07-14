7d7d8ec803bd37747e41ceed8beed69bf27fb412
Tina <tina.winnn@gmail.com>
2023-07-14 00:45:27 -0400

    added some comments to states

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -8,8 +8,11 @@ function Homepage() {
     // total_results object holds the total amount of photos that comes back on a search
     // create state photos to be stored
     const [photos, setPhotos] = useState([])
-    const [query, setQuery] = useState('god')
+    // setting state for query search
+    const [query, setQuery] = useState('')
+    // sets page one as default state for onload of both api fetches
     const [pageNum, setPageNum] = useState(1)
+    // sets 0 as initial and then updates to the total amount of pages available to display 
     const [totalPages, setTotalPages] = useState(0)
 
 // make api call when page loads for curated photos 
