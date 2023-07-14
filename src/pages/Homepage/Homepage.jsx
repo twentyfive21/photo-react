d4c290ec5fb52629e1ae40e3a6dc9446018782b8
Tina <tina.winnn@gmail.com>
2023-07-13 22:14:27 -0400

    added the curated fetch back

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -5,37 +5,62 @@ import PhotoCard from '../../components/PhotoCard/PhotoCard'
 
 
 function Homepage() {
-
+    // total_results object holds the total amount of photos that comes back on a search
     // create state photos to be stored
     const [photos, setPhotos] = useState([])
-    let pageNum = Math.floor(Math.random() * 50);
-    const [query, setQuery] = useState('mexicans eating')
-    console.log(query)
-   
-// Make a api call to site
-useEffect(() => {
+    const [query, setQuery] = useState('god')
+    const [pageNum, setPageNum] = useState(1)
+    const [totalPages, setTotalPages] = useState(0)
 
-
-  const fetchData = async () => {
-    try {
-      const apiKey = import.meta.env.VITE_API_KEY
-      const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${pageNum}&per_page=80`, {
-        headers: {
-          Authorization: apiKey
+// make api call when page loads for curated photos 
+    useEffect(() => {
+      // setPageNum(Math.floor(Math.random() * 3)); 
+      const fetchData = async () => {
+        try {
+          const apiKey = import.meta.env.VITE_API_KEY
+          const response = await fetch(`https://api.pexels.com/v1/curated?page=${pageNum}&per_page=80`, {
+            headers: {
+              Authorization: apiKey
+            }
+          });
+          const jsonData = await response.json();
+          console.log(jsonData)
+          setPhotos(jsonData?.photos)
+          setTotalPages(jsonData.total_results)
+          console.log(totalPages)
+        } catch (err) {
+          console.error(err);
         }
-      });
-      const jsonData = await response.json();
-      // console.log(jsonData.photos)
-      setPhotos(jsonData?.photos)
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  fetchData();
-}, []);
+      };
+      fetchData();
+    }, []);
+
+// Make a api call to site for search query 
+// useEffect(() => {
+
+//   // setPageNum(Math.floor(Math.random() * 3)); 
+//   // console.log(pageNum) 
+  
+//   const fetchData = async () => {
+//     try {
+//       const apiKey = import.meta.env.VITE_API_KEY
+//       const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&page={pageNum}&per_page=80`, {
+//         headers: {
+//           Authorization: apiKey
+//         }
+//       });
+//       const jsonData = await response.json();
+//       console.log(jsonData)
+//       setPhotos(jsonData?.photos)
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+//   fetchData();
+// }, [query]);
 
 // `https://api.pexels.com/v1/search?query=${cat}&page=${pageNum}&per_page=80`
-// https://api.pexels.com/v1/curated?page=80&per_page=40
+
 
 
   return (
