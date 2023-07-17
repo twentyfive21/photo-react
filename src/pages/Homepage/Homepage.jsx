5b3de34ab9775a08d0b3e55e68ecde15b9bdd003
Tina <tina.winnn@gmail.com>
2023-07-16 21:04:02 -0400

    added scroll to top func

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react'
 import './Homepage.css'
-import axios from 'axios'
 import PhotoCard from '../../components/PhotoCard/PhotoCard'
 
 
@@ -10,15 +9,29 @@ function Homepage() {
     // create state photos to be stored
     const [photos, setPhotos] = useState([])
       // setting state for query search
-      const [query, setQuery] = useState('')
+    const [query, setQuery] = useState('')
     // sets page one as default state for onload of both api fetches
     const [pageNum, setPageNum] = useState(1)
     // sets 0 as initial and then updates to the total amount of pages available to display 
     const [totalPages, setTotalPages] = useState(0)
+    
     // navigates through pages 
-      const nextPage = () => pageNum < totalPages ? setPageNum(pageNum + 1) : setPageNum(totalPages)
-      const prevPage = () => pageNum == 1 ? setPageNum(1) : setPageNum(pageNum - 1)
-      console.log("curr  total pages"+ totalPages)
+      const nextPage = () => {
+        scrollToTop()
+        pageNum < totalPages ? setPageNum(pageNum + 1) : setPageNum(totalPages)
+      }
+
+      const prevPage = () => {
+        scrollToTop()
+        pageNum == 1 ? setPageNum(1) : setPageNum(pageNum - 1)
+      }
+     
+      const scrollToTop = () => {
+        window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+      });
+}
 
 // Make a api call to site for search query 
 useEffect(() => {
@@ -53,7 +66,7 @@ else {
         }
       });
       const jsonData = await response.json();
-      // console.log(jsonData)
+      console.log(jsonData)
       setPhotos(jsonData?.photos)
       // since there are 8000 photos diving by 80 gives us 100pgs for curated 
       setTotalPages(Math.ceil(jsonData?.total_results / 80))
@@ -67,6 +80,7 @@ else {
 }, [query, pageNum]);
 
 
+
   return (
     <div>
      <div className='homepage-container'>
@@ -74,9 +88,12 @@ else {
         photos?.map(item => <PhotoCard key={item.id} photo={item}/>)
       }
       </div>
+      <div className='pages'>
+        <p>{pageNum} out of {totalPages}</p>
       <div className='homepage-btn'>
         <button onClick={prevPage}>Previous</button>
         <button onClick={nextPage}>Next</button>
+      </div>
       </div>
     </div>
     
