93b8581e3d4906422f64434dd057824a85561756
Tina <tina.winnn@gmail.com>
2023-07-19 21:46:58 -0400

    created context for theme and state

--- src/contexts/Theme.jsx
+++ src/contexts/Theme.jsx
@@ -1,2 +1,16 @@
+import {useState, createContext} from 'react'
 import { TbMoonStars } from "react-icons/tb";
 import { MdSunny } from "react-icons/md";
+import { ThemeContext } from './Theme';
+
+export const ThemeContext = createContext()
+
+export default function ThemeContextProvider(props) {
+    const [darkMode, setDarkMode] = useState(false)
+
+    return(
+        <ThemeContext.Provider value={{darkMode, setDarkMode}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
