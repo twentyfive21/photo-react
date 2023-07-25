bfddada4986f72fa44b41b96011a245ae88ff36e
Tina <tina.winnn@gmail.com>
2023-07-24 21:44:40 -0400

    added return for provider

--- src/contexts/Favorites.jsx
+++ src/contexts/Favorites.jsx
@@ -7,4 +7,9 @@ export const FavoritesContext = createContext()
 export default function FavoritesContextProvider(props){
     const [favorites, setFavorites] = useState([])
 
+    return(
+        <FavoritesContext.Provider value={{favorites, setFavorites}}>
+            {props.children}
+        </FavoritesContext.Provider>
+    )
 }
