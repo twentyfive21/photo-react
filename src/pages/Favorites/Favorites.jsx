ebc8280af74bd74462ecc7bd54dffd8cc205ddaf
Tina <tina.winnn@gmail.com>
2023-07-27 13:48:55 -0400

    added theme context to favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -2,14 +2,16 @@ import React, {useContext} from 'react'
 import './Favorites.css'
 import { FavoritesContext } from '../../contexts/FavoritesContext'
 import PhotoCard from '../../components/PhotoCard/PhotoCard'
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 function Favorites() {
   // display fav photos get from context 
     // access favorites global context use {} not []
+    const {darkMode, setDarkMode} = useContext(ThemeContext)
     const {favorites} = useContext(FavoritesContext)
   return (
-    <div className='favorites-container'>
-      <h1>My Palette</h1> 
+    <div className={darkMode?'favorites-container favorites-dark' : 'favorites-container'}>
+      <h1>My Current Palette</h1> 
       <div className='favorites-center'>
         {
           favorites.map(item => <PhotoCard key={item.id} photo={item}/>)
