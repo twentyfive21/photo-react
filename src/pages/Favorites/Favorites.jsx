2b731cf40f8fe850d9789da9e48b4c9729757b0c
Tina <tina.winnn@gmail.com>
2023-07-26 22:09:07 -0400

    added map func for cards in favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './Favorites.css'
+import { FavoritesContext } from '../../contexts/FavoritesContext'
+import PhotoCard from '../../components/PhotoCard/PhotoCard'
 
 function Favorites() {
+  // display fav photos get from context 
+    // access favorites global context use {} not []
+    const {favorites} = useContext(FavoritesContext)
   return (
-    <div>Favorites</div>
+    <div className='favorites-container'>
+      <h1>My Palette</h1>
+      <div className='favorites-center'>
+        {
+          favorites.map(item => <PhotoCard key={item.id} photo={item}/>)
+        }
+      </div>
+    </div>
   )
 }
 
